aa674f0a032e39520e3352a9b84ec189cfda7bad
prudhvi <prudhvi2121991@gmail.com>
2017-11-26 22:06:27 +0530

    Final code for preprocess file using all features

--- preprocess.py
+++ preprocess.py
@@ -16,7 +16,14 @@ from nltk.corpus import stopwords
 stops = stopwords.words('english')
 import nltk
 from nltk import word_tokenize
-
+import operator
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
 os.chdir('/home/prudhvi/Documents')
 
 emails = pd.read_csv('enron_emails.csv')
@@ -138,3 +145,53 @@ list_of_test_authors = ['beck-s','farmer-d','kaminski-v','kitchen-l','lokay-m','
 emails_sample = emails[emails['user'].isin(list_of_test_authors)]
 
 
+kk = dict(Counter(emails_sample['X-From']))
+
+sorted_x = sorted(kk.iteritems(), key=operator.itemgetter(1), reverse= True)
+
+list_of_X_from = ['Sally Beck','Richard B Sanders','Daren J Farmer','Kaminski, Vince J </O=ENRON/OU=NA/CN=RECIPIENTS/CN=VKAMINS>','Kitchen, Louise </O=ENRON/OU=NA/CN=RECIPIENTS/CN=LKITCHEN>','Lokay, Michelle </O=ENRON/OU=NA/CN=RECIPIENTS/CN=MLOKAY>','Williams III, Bill </O=ENRON/OU=NA/CN=RECIPIENTS/CN=BWILLIA5>']
+
+emails_sample = emails_sample[emails_sample['X-From'].isin(list_of_X_from)]
+
+
+final_df = emails_sample.iloc[:,[18,19,20,21,22,23,24,25,26,27]]
+
+target = emails_sample.iloc[:,9]
+
+le = preprocessing.LabelEncoder()
+
+labelled_target = le.fit_transform(target)
+
+
+
+X_train,X_test,Y_train,Y_test = train_test_split(final_df,target,test_size= 0.33)
+
+rf = RandomForestClassifier( random_state=0)
+
+parameters = [{ 'n_estimators' : [10,30,50], 'min_samples_split' : [0.1,0.3] , 'min_samples_leaf' : [5,15,20] , 'max_features': [3,5,7] , 'max_depth' :[10,15,30]}]
+
+clf = GridSearchCV(rf, param_grid= parameters, cv=5,
+                   scoring= 'accuracy')
+
+clf.fit(X_train,Y_train)
+
+print clf.best_params_
+
+
+rf = RandomForestClassifier(max_features= 8, random_state=0)
+
+rf.fit(X_train, Y_train)
+
+preds = rf.predict(X_test)
+
+
+
+accuracy_score(Y_test,preds)
+print classification_report(Y_test,preds)
+
+cm = confusion_matrix(Y_test,preds)
+print cm
+
+for i in range(len(cm)):
+    val = float(cm[i][i]) / float((cm[i][0] + cm[i][1] + cm[i][2] + cm[i][3] + cm[i][4] + cm[i][5] + cm[i][6]))
+    print val
